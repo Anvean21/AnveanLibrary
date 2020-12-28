9d6505b6ca43839b4250592a265ed0c939d66072
Anvean <evgeniy.kovalenko21@gmail.com>
2020-12-28 11:01:11 +0200

    Add project files.

---
+++ AnveanLibrary/ArrayWorker.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnveanLibrary
+{
+   public class ArrayWorker
+    {
+        //Поиск индекса числа в массиве
+        static void Find(int[] array, int nubmer)
+        {
+            string index = "";
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == nubmer)
+                {
+                    index += Convert.ToString(i) + " ";
+
+                }
+            }
+            Console.WriteLine($"Индексы числа {nubmer} = {index} ");
+        }
+
+
+        //Сортивровка двумерного массива по столбцам или по строкам
+        private static void SortCOL(int[,] arr, int ROW, int COL)
+        {
+            // ROW COL должны быть константы. (Строки, Столбцы)
+            for (int i = 0; i < ROW; i++)
+            {
+                for (int j = 0; j < COL - 1; j++)
+                {
+                    for (int k = 0; k < COL - j - 1; k++)
+                    {
+                        if (arr[i, k] > arr[i, k + 1]) //Если поменять знак, поменяется и сортировка
+                        {
+                            int temp = arr[i, k];
+                            arr[i, k] = arr[i, k + 1];
+                            arr[i, k + 1] = temp;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        //Обычная сортировка массива Пузырьком
+        private static void Sort(int[] nums)
+        {
+            int temp;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+        }
+
+
+        //Заполнение 1(2) мерного массива случайными числами
+        private static void Fill(int[,] matrix)
+        {
+            var rnd = new Random();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = rnd.Next(100);
+                }
+            }
+        }
+
+
+        //Заполнение 2мерного массива ЗМЕЙКОЙ 1 2 3 4 5
+        //                                   10 9 8 7 6
+       static void FillSnake(int[,] array)
+        {
+            int n = array.GetLength(0), m = array.GetLength(1);
+            for (int i = 0, v = 0; i < n + m - 1; i++)
+            {
+                int r, c;
+                if (i % 2 == 1)
+                {
+                    r = i < m ? 0 : i - m + 1;
+                    c = i < m ? i : m - 1;
+                }
+                else
+                {
+                    r = i < n ? i : n - 1;
+                    c = i < n ? 0 : i - n + 1;
+                }
+                while (r < n && r >= 0 && c < m && c >= 0)
+                {
+                    array[i % 2 == 0 ? r-- : r++, i % 2 == 0 ? c++ : c--] = ++v;
+                }
+            }
+        }
+
+
+        //Вывод массива 1(2) мерного
+        private static void Print(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+
+        //Нахождение минимального числа в массиве []/[,]
+        private static int Min(int[,] matrix)
+        {
+            var min = int.MaxValue;
+
+            foreach (var value in matrix)
+            {
+                if (min > value) min = value;
+            }
+
+            return min;
+        }
+
+
+        //Нахождение максимального числа в массиве []/[,]
+        private static int Max(int[,] matrix)
+        {
+            var max = int.MinValue;
+
+            foreach (var value in matrix)
+            {
+                if (value > max) max = value;
+            }
+
+            return max;
+        }
+
+
+        //Поиск индекса по ссылке ПРИМЕР В КОММЕНТАРИЯХ МЕТОДА
+        static ref int Find(int number, int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == number)
+                {
+                    return ref numbers[i]; // возвращаем ссылку на адрес, а не само значение
+                }
+            }
+            throw new IndexOutOfRangeException("число не найдено");
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+            //ref int numberRef = ref Find(4, numbers); // ищем число 4 в массиве
+            //numberRef = 9; // заменяем 4 на 9
+            //Console.WriteLine(numbers[3]); // 9
+
+            //Console.Read();
+        }
+    }
+
+}
+
---
+++ AnveanLibrary/StringWorker.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnveanLibrary
+{
+   public class StringWorker
+    {
+        
+        // Убрать из массива строк строки по определённому критерию
+        static string[] array = array.Where(line => !line.Contains("Microsoft Visual C++ ")).ToArray();
+
+        // Создать массив на основании дубликатов первого массива
+        // То есть, если в первом массиве идут цифры { 1, 2, 3, 4, 4, 5, 5}, то второй будет = { 4, 5 } 
+        static string[] array2 = array2.GroupBy(x => x).Where(g => g.Count() > 1).Select(g => g.Key).ToArray();
+
+        // Преобразовать массив в строку
+       static string str = string.Join("\r\n", array);
+
+        // Преобразовать строку в массив
+        string[] array3 = str.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+       
+
+
+        //Разные методы работы со строками
+        public static void StringMethdos()
+        {
+            // Отсортировать массив по алфавиту
+            Array.Sort(array);
+
+            // Массив строк минус дубликаты
+            array.Distinct().ToArray();
+
+            // Удалить всё, что идёт после определённого символа, не оставляя при этом сам символ
+            string text = "Some text";
+            text.Remove(text.IndexOf(" "));
+
+            // Удалить всё, что идёт после определённого символа, оставив при этом сам символ
+            text.Remove(text.IndexOf(" ") + 1);
+
+            // Удалить из строки всё, что идёт до определённого символа, оставив при этом определённый символ
+            text.Substring(text.IndexOf(" "));
+
+            // Удалить из строки всё, что идёт до определённого символа, не оставляя при этом определённый символ
+            text.Substring(text.IndexOf(" ") + 1);
+
+            // Удалить из строки всё, что идёт от определённого символа, удалив при этом определённый символ
+            text.Substring(0, text.IndexOf(" "));
+
+            // Удалить из строки всё, что идёт до последнего определённого символа, не удаляя сам символ
+            text.Substring(text.LastIndexOf(" "));
+
+            // Удалить из строки всё, что идёт после последнего определённого символа, удалив при этом определённый символ
+            text.Substring(0, text.LastIndexOf(" "));
+
+            // Удалить из строки всё, что идёт до последнего определённого символа, удалив при этом сам символ
+            text.Substring(text.LastIndexOf(" ") + 1);
+
+            // Удалить из строки всё, что идёт после последнего определённого символа, оставив при этом сам символ
+            text.Substring(0, text.LastIndexOf(" ") + 1);
+        } 
+    }
+    
+    public static class StringExtentio
+    {
+        //Метод расширения для подсчета букв в строке
+        public static int CharCounter(this string str, char c)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == c)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
